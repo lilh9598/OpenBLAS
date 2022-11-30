17d87d6d012811d4997b9e8a861ed7b071c33457
root <root@debian.debian>
2022-11-30 11:44:38 +0800

    kernel 8x8 for sbgemm on n2

--- param.h
+++ param.h
@@ -3401,8 +3401,8 @@ is a big desktop or server with abundant cache rather than a phone or embedded d
 
 #undef SBGEMM_DEFAULT_UNROLL_M
 #undef SBGEMM_DEFAULT_UNROLL_N
-#define SBGEMM_DEFAULT_UNROLL_M 8
-#define SBGEMM_DEFAULT_UNROLL_N 4
+#define SBGEMM_DEFAULT_UNROLL_M  8
+#define SBGEMM_DEFAULT_UNROLL_N  8
 
 #define SGEMM_DEFAULT_UNROLL_M  16
 #define SGEMM_DEFAULT_UNROLL_N  4
